baaf3d6eb96bd46a8af17128d29af9b0b93eede8
tanzilrayhan <tanzilrayhan169@gmail.com>
2025-01-05 19:07:54 +0600

    login check fixed

--- Project/StudyCompass/controller/loginCheck.php
+++ Project/StudyCompass/controller/loginCheck.php
@@ -1,31 +1,31 @@
-<?php 
+<?php
 session_start();
 require_once('../model/authModel.php');
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
 
     if (empty($username) || empty($password)) {
-        echo "Username and Password cannot be empty!";
+        $_SESSION['error'] = "Username and Password cannot be empty!";
+        header('location: ../view/login.php');
+        exit();
     } else {
-        // Check for user in admin table
-        $admin = getAdmin($username, $password);
-        // Check for user in users table
-        $user = getUser($username, $password);
+        $admin = getAdmin($username);
+        $user = getUser($username);
 
-        if ($admin) {
+        if ($admin && $password === $admin['password']) {
             $_SESSION['username'] = $username;
             setcookie('status', 'true', time() + 10000, '/');
             header('location: ../view/adminDashboard.php');
             exit();
-        } elseif ($user) {
+        } elseif ($user && $password === $user['password']) {
             $_SESSION['username'] = $username;
             setcookie('status', 'true', time() + 10000, '/');
             header('location: ../view/userDashboard.php');
             exit();
         } else {
-            echo "Invalid username or password!";
+            $_SESSION['error'] = "Invalid username or password!";
             header('location: ../view/login.php');
             exit();
         }
@@ -34,4 +34,3 @@ if(isset($_POST['submit'])) {
     header('location: ../view/login.php');
     exit();
 }
-?>
